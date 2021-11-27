a404b80c4447f831f8bd973bcc0fddf0e5918864
ads12612 <eric.soucy@ssq.ca>
2021-11-26 19:23:21 -0500

    Initial commit

---
+++ src/main/java/com/example/demojsf/DemoJsfApplication.java
@@ -0,0 +1,36 @@
+package com.example.demojsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.faces.webapp.FacesServlet;
+import javax.servlet.ServletContext;
+import java.util.Arrays;
+
+@SpringBootApplication
+public class DemoJsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJsfApplication.class, args);
+	}
+
+	@Bean
+	ServletRegistrationBean jsfServletRegistration (ServletContext servletContext) {
+		//spring boot only works if this is set
+		servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+
+		//FacesServlet registration
+		ServletRegistrationBean srb = new ServletRegistrationBean();
+		srb.setServlet(new FacesServlet());
+		srb.setUrlMappings(Arrays.asList("*.xhtml"));
+		srb.setLoadOnStartup(1);
+		return srb;
+	}
+
+	@Bean
+	public com.sun.faces.config.ConfigureListener mojarraConfigureListener() {
+		return new com.sun.faces.config.ConfigureListener();
+	}
+}
---
+++ src/test/java/com/example/demojsf/DemoJsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
